7a6a74668cc68441a3485172acbf2baa46ebc851
dinhkhiemtran <dinhkhiem230195@gmail.com>
2022-11-23 00:05:48 +0700

    update Tree Set

--- src/main/java/com/example/javabasic/collections/collection/set/TreeSet.java
+++ src/main/java/com/example/javabasic/collections/collection/set/TreeSet.java
@@ -186,7 +186,8 @@ public class TreeSet implements NavigableSet {
         Scanner scanner = new Scanner(System.in);
         java.util.TreeSet<String> treeSet = addElementToTreeSetString();
         try{
-            System.out.print("Input element that you want to remove from tree set: ");
+            accessTreeSetElements(treeSet);
+            System.out.print("Input element that you want to remove from tree set String: ");
             String element = scanner.next();
             treeSet.remove(element);
         }catch (InputMismatchException inputMismatchException){
@@ -211,7 +212,8 @@ public class TreeSet implements NavigableSet {
         Scanner scanner = new Scanner(System.in);
         java.util.TreeSet<Integer> treeSet = addElementToTreeSetInteger();
         try{
-            System.out.print("Input element that you want to remove from tree set: ");
+            accessTreeSetElements(treeSet);
+            System.out.print("Input element that you want to remove from tree set Integer: ");
             Integer element = scanner.nextInt();
             treeSet.remove(element);
         }catch (InputMismatchException inputMismatchException){
@@ -266,6 +268,94 @@ public class TreeSet implements NavigableSet {
         return results;
     }
 
+    public java.util.TreeSet<String> getTailSetString(){
+        Scanner scanner = new Scanner(System.in);
+        java.util.TreeSet<String> treeSet = addElementToTreeSetString();
+        java.util.TreeSet<String> results = new java.util.TreeSet<>();
+        try{
+            if (treeSet != null){
+                if (!treeSet.isEmpty()){
+                    accessTreeSetElements(treeSet);
+                    System.out.print("Input an element that you want to get tail set: ");
+                    String element = scanner.next();
+                    results = (java.util.TreeSet<String>) treeSet.tailSet(element);
+                }
+            }
+        }catch (InputMismatchException inputMismatchException){
+            System.out.println("Exception: " + "Input wrong type.");
+        }catch (Exception exception){
+            System.out.println("Exception: " + exception.getMessage());
+        }
+        return results;
+    }
+
+    public java.util.TreeSet<Integer> getTailSetInteger(){
+        Scanner scanner = new Scanner(System.in);
+        java.util.TreeSet<Integer> treeSet = addElementToTreeSetInteger();
+        java.util.TreeSet<Integer> results = new java.util.TreeSet<>();
+        try{
+            if (treeSet != null){
+                if (!treeSet.isEmpty()){
+                    accessTreeSetElements(treeSet);
+                    System.out.print("Input an element that you want to get tail set: ");
+                    Integer element = scanner.nextInt();
+                    results = (java.util.TreeSet<Integer>) treeSet.tailSet(element);
+                }
+            }
+        }catch (InputMismatchException inputMismatchException){
+            System.out.println("Exception: " + "Input wrong type.");
+        }catch (Exception exception){
+            System.out.println("Exception: " + exception.getMessage());
+        }
+        return results;
+    }
+
+    public java.util.TreeSet<String> getSubSetString(){
+        Scanner scanner = new Scanner(System.in);
+        java.util.TreeSet<String> treeSet = addElementToTreeSetString();
+        java.util.TreeSet<String> results = new java.util.TreeSet<>();
+        try{
+            if (treeSet != null){
+                if (!treeSet.isEmpty()){
+                    accessTreeSetElements(treeSet);
+                    System.out.print("Input an element start String: ");
+                    String element1 = scanner.next();
+                    System.out.print("Input an element end String: ");
+                    String element2 = scanner.next();
+                    results = (java.util.TreeSet<String>) treeSet.subSet(element1,true,element2,true);
+                }
+            }
+        }catch (InputMismatchException inputMismatchException){
+            System.out.println("Exception: " + "Input wrong type.");
+        }catch (Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }
+        return results;
+    }
+
+    public java.util.TreeSet<Integer> getSubSetInteger(){
+        Scanner scanner = new Scanner(System.in);
+        java.util.TreeSet<Integer> treeSet = addElementToTreeSetInteger();
+        java.util.TreeSet<Integer> results = new java.util.TreeSet<>();
+        try{
+            if (treeSet != null){
+                if (!treeSet.isEmpty()){
+                    accessTreeSetElements(treeSet);
+                    System.out.print("Input an element start Integer: ");
+                    Integer element1 = scanner.nextInt();
+                    System.out.print("Input an element end Integer: ");
+                    Integer element2 = scanner.nextInt();
+                    results = (java.util.TreeSet<Integer>) treeSet.subSet(element1,true, element2,true);
+                }
+            }
+        }catch (InputMismatchException inputMismatchException){
+            System.out.println("Exception: " + "Input wrong type.");
+        }catch (Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }
+        return results;
+    }
+
     public <T> T getFirstElement(java.util.TreeSet<T> treeSet){
         return treeSet.first();
     }
@@ -282,6 +372,49 @@ public class TreeSet implements NavigableSet {
         return treeSet.pollLast();
     }
 
+    public <T>java.util.TreeSet<T> unionTreeSet(java.util.TreeSet<T> set1, java.util.TreeSet<T> set2){
+        set1.addAll(set2);
+        return set1;
+    }
+
+    public <T>java.util.TreeSet<T> intersectionTreeSet(java.util.TreeSet<T> set1, java.util.TreeSet<T> set2){
+        set1.retainAll(set2);
+        return set1;
+    }
+
+    public <T>java.util.TreeSet<T> differenceTreeSet(java.util.TreeSet<T> set1, java.util.TreeSet<T> set2){
+        set1.removeAll(set2);
+        return set1;
+    }
+
+    public <T> boolean subSet(java.util.TreeSet<T> set1, java.util.TreeSet<T> set2){
+        return set1.containsAll(set2);
+    }
+
+    public java.util.TreeSet<String> addElementToOriginSetString(){
+        java.util.TreeSet<String> origin = new java.util.TreeSet<>();
+        origin.add("SpringBoot");
+        origin.add("WPF");
+        origin.add("Laravel");
+        origin.add("Rails");
+        origin.add("Express");
+        origin.add("Java");
+        return origin;
+    }
+
+    public java.util.TreeSet<Integer> addElementToOriginSetInteger(){
+        java.util.TreeSet<Integer> origin = new java.util.TreeSet<>();
+        for (int i = 10 ; i <= 20 ; i++){
+            origin.add(i);
+        }
+        return origin;
+    }
+
+    public <T>java.util.TreeSet<T> removeAll(java.util.TreeSet<T> treeSet){
+        treeSet.clear();
+        return treeSet;
+    }
+
     public void load(){
         System.out.println(addElementToTreeSetString());
         System.out.println(addElementToTreeSetInteger());
@@ -293,10 +426,26 @@ public class TreeSet implements NavigableSet {
         System.out.println("Last element: " + getLastElement(addElementToTreeSetInteger()));
         System.out.println("First element is removed : " + removeFirstElement(addElementToTreeSetString()));
         System.out.println("First element is removed : " + removeFirstElement(addElementToTreeSetInteger()));
-        System.out.println("Last element is removed : " + removeLastElement(addElementToTreeSetInteger()));
+        System.out.println("Last element is removed : " + removeLastElement(addElementToTreeSetString()));
         System.out.println("Last element is removed : " + removeLastElement(addElementToTreeSetInteger()));
         System.out.println("Head Set: " + getHeadSetFromTreeSetString());
         System.out.println("Head Set: " + getHeadSetFromTreeSetInteger());
+        System.out.println("Tail Set: " + getTailSetString());
+        System.out.println("Tail Set: " + getTailSetInteger());
+        System.out.println("Sub Set: " + getSubSetString());
+        System.out.println("Sub Set: " + getSubSetInteger());
+        System.out.println("Union: " + unionTreeSet(addElementToOriginSetString(), addElementToTreeSetString()));
+        System.out.println("Union: " + unionTreeSet(addElementToOriginSetInteger(), addElementToTreeSetInteger()));
+        System.out.println("Intersection: " + intersectionTreeSet(addElementToOriginSetString(), addElementToTreeSetString()));
+        System.out.println("Intersection: " + intersectionTreeSet(addElementToOriginSetInteger(), addElementToTreeSetInteger()));
+        System.out.println("Difference: " + differenceTreeSet(addElementToOriginSetString(), addElementToTreeSetString()));
+        System.out.println("Difference: " + differenceTreeSet(addElementToOriginSetInteger(), addElementToTreeSetInteger()));
+        System.out.println("Sub Set: " + subSet(addElementToOriginSetString(), addElementToTreeSetString()));
+        System.out.println("Sub Set: " + subSet(addElementToOriginSetInteger(), addElementToTreeSetInteger()));
+        System.out.println(removeElementFromTreeSetInteger());
+        System.out.println(removeElementFromTreeSetString());
+        System.out.println(removeAll(addElementToTreeSetString()));
+        System.out.println(removeAll(addElementToTreeSetInteger()));
     }
 
     public static void main(String[] args) {
